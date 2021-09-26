d91bc9fdf50c161b786f62778abc45ff665ccf8c
StartUpTech21 <58793672+widiarrohman1234@users.noreply.github.com>
2021-09-26 17:31:02 +0700

    Create array.js

---
+++ BootcampReactNative/array.js
@@ -0,0 +1,13 @@
+function range(startNum, finishNum){
+    var result = startNum + finishNum
+    return result
+}
+
+var hasil = range([0,1,2]])
+console.log(hasil)
+
+// console.log(range(1, 10)) //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(range(1)) // -1
+// console.log(range(11,18)) // [11, 12, 13, 14, 15, 16, 17, 18]
+// console.log(range(54, 50)) // [54, 53, 52, 51, 50]
+// console.log(range()) // -1 
