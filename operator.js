2c81357a220c21630f05e3a00551efe47efbd62d
StartUpTech21 <58793672+widiarrohman1234@users.noreply.github.com>
2021-09-26 16:08:34 +0700

    Update operator.js

--- operator.js
+++ operator.js
@@ -1,4 +1,11 @@
 // Equal Operator (==)
 let equalOperator = 100
+console.log("equalOperator")
 console.log(equalOperator == 100) // true
 console.log(equalOperator == 20) // false
+
+// Not Equal ( != )
+var notEqual = "rajin"
+console.log("notEqual")
+console.log(notEqual != "malas") // true
+console.log(notEqual != "pandai") //true 
