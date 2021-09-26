207c0fa2ae6077f754e185257c0177ab59ffd2c0
StartUpTech21 <58793672+widiarrohman1234@users.noreply.github.com>
2021-09-26 16:30:50 +0700

    Create array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+// property .length
+var hobbies = ["coding", "bersepeda", "mengaji", "belajar"] 
+console.log(hobbies) // ["coding", "bersepeda", "mengaji", "belajar"] 
+console.log(hobbies.length) // 4 
+ 
+console.log(hobbies[0]) // coding
+console.log(hobbies[2]) // mengaji
+// Mengakses elemen terakhir dari array
+console.log(hobbies[hobbies.length -1]) // belajar
