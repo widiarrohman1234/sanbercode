2c5d535bc3d04474a746dbef12d8396aa7b86914
StartUpTech21 <58793672+widiarrohman1234@users.noreply.github.com>
2021-09-26 16:04:29 +0700

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,7 +1,10 @@
 let name = "widi" // Tipe
 let angka = 9
 let hariIniSenin = false 
+var data
+
 
 console.log(name) // "John"
 console.log(angka) // 12
 console.log(hariIniSenin) // false
+console.log(data) // Undefined
